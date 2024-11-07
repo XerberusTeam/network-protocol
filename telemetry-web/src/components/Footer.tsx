1c7f8bae2e9cb0dbeddc7b14863ad9928ecfb117
Evangelos Pappas <epappas@evalonlabs.com>
2024-11-07 09:38:56 +0000

    (telemetry-web) fix wrong link to app

--- telemetry-web/src/components/Footer.tsx
+++ telemetry-web/src/components/Footer.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
-import './Footer.css';
 import logo from '../../assets/logo.svg';
+import './Footer.css';
 
 export class Footer extends React.Component {
   public render() {
     return (
       <footer className="Footer">
         <div className="Footer-content">
-        <img src={logo} alt="Telemetry Logo" />
+          <img src={logo} alt="Telemetry Logo" />
           {/* <p>© 2024 Xerberus DAO LLC. All rights reserved.</p> */}
           <div className="Footer-links">
-          <a href="https://www.app.xerberus.io" target="_blank" rel="noopener noreferrer">App</a>
+            <a href="https://app.xerberus.io" target="_blank" rel="noopener noreferrer">App</a>
             <a href="https://www.xerberus.io" target="_blank" rel="noopener noreferrer">Website</a>
-           
+
           </div>
         </div>
       </footer>
